14c015194a35af1c95a7bec99ede97c2bf5b125a
martinRenou <martin.renou@gmail.com>
2020-09-08 09:57:01 +0200

    Compile AST
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- xrobotkernel/install.py
+++ xrobotkernel/install.py
@@ -21,7 +21,7 @@ def install_my_kernel_spec(user=False, prefix=None):
         # TODO: Copy any resources
 
         print('Installing Jupyter kernel spec')
-        KernelSpecManager().install_kernel_spec(td, 'echo', user=user, replace=True, prefix=prefix)
+        KernelSpecManager().install_kernel_spec(td, 'echo', user=user, prefix=prefix)
 
 
 def _is_root():
--- xrobotkernel/kernel.py
+++ xrobotkernel/kernel.py
@@ -1,5 +1,6 @@
 """XRobotKernel."""
 from ipykernel.kernelbase import Kernel
+from robot.api import get_tokens
 
 
 class XRobotKernel(Kernel):
@@ -18,8 +19,13 @@ class XRobotKernel(Kernel):
 
 
     def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
+        output = ''
+
+        for token in get_tokens(code):
+            output += repr(token) + '\n'
+
         if not silent:
-            stream_content = {'name': 'stdout', 'text': code}
+            stream_content = {'name': 'stdout', 'text': output}
             self.send_response(self.iopub_socket, 'stream', stream_content)
 
         return {
