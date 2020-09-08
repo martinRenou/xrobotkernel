6e7421144d08808123c1509d808ca7c814f2053e
martinRenou <martin.renou@gmail.com>
2020-09-08 10:20:27 +0200

    Keep the context in memory
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- xrobotkernel/kernel.py
+++ xrobotkernel/kernel.py
@@ -1,6 +1,8 @@
 """XRobotKernel."""
 from ipykernel.kernelbase import Kernel
 from robot.api import get_tokens
+from robot.parsing.lexer.context import TestCaseFileContext
+from robot.parsing.lexer.lexer import Lexer
 
 
 class XRobotKernel(Kernel):
@@ -8,20 +10,29 @@ class XRobotKernel(Kernel):
 
     implementation = 'XRobot'
     implementation_version = '0.1'
-    language = 'no-op'
+    language = 'robot'
     language_version = '0.1'
     language_info = {
-        'name': 'Any text',
+        'name': 'robot',
         'mimetype': 'text/plain',
-        'file_extension': '.txt',
+        'file_extension': '.robot',
     }
     banner = "XRobot kernel"
 
 
+    def __init__(self, *args, **kwargs):
+        self.context = TestCaseFileContext()
+        self.lexer = Lexer(self.context, False, False)
+
+        super(XRobotKernel, self).__init__(*args, **kwargs)
+
+
     def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
+        self.lexer.input(code)
+
         output = ''
 
-        for token in get_tokens(code):
+        for token in self.lexer.get_tokens():
             output += repr(token) + '\n'
 
         if not silent:
